dd23cd7195efdc6005d5a47368a23e89f13eaaf7
Darion Kellar <credkellar@gmail.com>
2026-06-19 20:35:38 -0700

    Delete aerospace-layer directory

--- aerospace-layer/thrust_vectoring.py
+++
@@ -1,9 +0,0 @@
-def set_thrust_vectors(mode, vector_coords):
-    """
-    Adjusts engine nozzles or rotor pitch.
-    VTOL modes require complex vectoring; Jet modes require directional exhaust.
-    """
-    if mode in ["DRONE", "HELICOPTER"]:
-        return "ENGAGE_VTOL_MATRIX"
-    return "ENGAGE_LINEAR_THRUST"
-  

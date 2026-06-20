414815fe7eb708fddef911be33276f803a80df9f
Darion Kellar <credkellar@gmail.com>
2026-06-19 20:36:14 -0700

    Delete design-engine directory

--- design-engine/cad_exporter.py
+++
@@ -1,7 +0,0 @@
-# design-engine/cad_exporter.py
-
-def export_to_cad(design_specs, filename):
-    """Exports chassis design to standard CAD formats."""
-    print(f"Exporting {design_specs} to {filename}.step")
-    return True
-  
--- design-engine/interior_layout_gen.py
+++
@@ -1,8 +0,0 @@
-def set_cabin_dimensions(legroom_inches, seat_count):
-    """Configures interior CAD parameters."""
-    layout = {
-        "is_lwb": legroom_inches > 40,
-        "seats": seat_count
-    }
-    return layout
-  

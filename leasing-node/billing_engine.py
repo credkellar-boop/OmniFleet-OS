86b26015aaa2f8d6174535c31ea55c50dfc0b15f
Darion Kellar <credkellar@gmail.com>
2026-06-19 20:04:24 -0700

    Add function to calculate final invoice

---
+++ leasing-node/billing_engine.py
@@ -0,0 +1,6 @@
+def calculate_final_invoice(start_time, end_time, damages):
+    """Calculates total rental cost + potential fees."""
+    base_rate = 100
+    total = base_rate + damages
+    return total
+  

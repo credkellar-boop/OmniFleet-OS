fda4d9382223c53b0b428da39a521121e37966e5
Darion Kellar <credkellar@gmail.com>
2026-06-19 20:37:58 -0700

    Delete leasing-node directory

--- leasing-node/billing_engine.py
+++
@@ -1,6 +0,0 @@
-def calculate_final_invoice(start_time, end_time, damages):
-    """Calculates total rental cost + potential fees."""
-    base_rate = 100
-    total = base_rate + damages
-    return total
-  
